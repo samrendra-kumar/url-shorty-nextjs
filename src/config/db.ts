03bc2dd53a22c2a9f9e1955bbe9a9fe370dba4e4
Samrendra <samrendrakumar893@gmail.com>
2024-07-21 15:15:04 +0530

    url created

---
+++ src/config/db.ts
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const connectDB=async()=>{
+    console.log(process.env.MONGODB_URI)
+    return mongoose.connect(process.env.MONGODB_URI as string )
+}
+export default connectDB ;
